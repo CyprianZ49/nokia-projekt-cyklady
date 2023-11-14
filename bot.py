9cf516c80b1be668a436eedb0831f40a078d955f
CyprianZ49 <150189300+CyprianZ49@users.noreply.github.com>
2023-11-14 21:25:32 +0100

    zapomniałem save bota - ups

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 #graczy w sensie gry nazywami botami
 
 class Bot:
-    def __init__(self, filename='', nazwa):
-        self.id=nazwa
+    def __init__(self, name, filename=''):
+        self.name=name
         self.coins=0
         self.philosophers=0
         self.ownedTiles={}
     def __hash__(self):
-        return self.id
+        return hash(self.name)
     def get_move(self):
         return input().split()
     def send_move(self,player,move):
