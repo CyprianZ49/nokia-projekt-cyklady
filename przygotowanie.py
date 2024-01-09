6b0d6d87d74c7a37cb5dad6bd8a28e12688f2e36
qxyzpk <75301132+qxyzpk@users.noreply.github.com>
2024-01-09 20:45:17 +0100

    some helpful comments

--- przygotowanie.py
+++ przygotowanie.py
@@ -4,22 +4,22 @@ def przypiszWarunkiStartowe(board, players, ilosc):
     board.changeOwnership(7, 6, players[0])
     board.pola[7][6].strength+=1
     if ilosc > 0:
-        board.changeOwnership(1, 1, players[0])
+        board.changeOwnership(1, 1, players[0]) # water
         board.pola[1][1].strength+=1
-        board.changeOwnership(3, 3, players[0])
+        board.changeOwnership(3, 3, players[0]) # land
         board.pola[3][3].strength+=1
-        board.changeOwnership(9, 9, players[0])
+        board.changeOwnership(9, 9, players[0]) # water
         board.pola[9][9].strength+=1
-        board.changeOwnership(9, 10, players[0])
+        board.changeOwnership(9, 10, players[0]) #land
         board.pola[9][10].strength+=1
     if ilosc > 1:
-        board.changeOwnership(6, 1, players[1])
+        board.changeOwnership(6, 1, players[1]) # water
         board.pola[6][1].strength+=1
-        board.changeOwnership(5, 3, players[1])
+        board.changeOwnership(5, 3, players[1]) # land
         board.pola[5][3].strength+=1
-        board.changeOwnership(5, 6, players[1])
+        board.changeOwnership(5, 6, players[1]) # water
         board.pola[5][6].strength+=1
-        board.changeOwnership(6, 7, players[1])
+        board.changeOwnership(6, 7, players[1]) # land
         board.pola[6][7].strength+=1
     if ilosc > 2:
         board.changeOwnership(10, 5, players[2])
